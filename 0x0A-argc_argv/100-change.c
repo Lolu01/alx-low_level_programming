ea3030ae696398ff37b77af4334f153c2b69028c
Lolu01 <jaiyeolalulu@gmail.com>
2023-10-04 16:41:39 -0700

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,44 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * main - prints the minimum number of coins to
- * make change for an amount of money
- * @argc: number of arguments
- * @argv: array of arguments
- * 
- * Return: 0 (Success), 1 (Error)
+ * main - prints min num of coins to make change
+ * @argc: args count
+ * @argv: string array
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int num, j, result;
-	int coins[] = {25, 10, 5, 2, 1};
-
-	if (argc != 2)
-	{
-	       	printf("Error\n");
-		return (1);
-	}
-
-	num = atoi(argv[1]);
-	result = 0;
-
-	if (num < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (j = 0; j < 5 && num >= 0; j++)
-	{
-		while (num >= coins[j])
-		{
-			result++;
-			num -= coins[j];
-		}
-	}
-
-	printf("%d\n", result);
-	return (0);
+int cents;
+int quarters, dimes, nickels, pennies, twoPenny;
+if (argc != 2)
+{
+puts("Error");
+return (1);
+}
+cents = atoi(argv[1]);
+if (cents < 0)
+{
+puts("0");
+return (0);
+}
+quarters = cents / 25;
+cents = cents - quarters * 25;
+dimes = cents / 10;
+cents = cents - dimes * 10;
+nickels = cents / 5;
+cents = cents - nickels * 5;
+twoPenny = cents / 2;
+cents = cents - twoPenny * 2;
+pennies = cents / 1;
+cents = cents - pennies;
+printf("%d\n", quarters + dimes + nickels + twoPenny +  pennies);
+return (0);
 }
