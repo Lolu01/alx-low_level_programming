2e05a342f3c7ea5defd5d1c36740abb4b2235ae0
Lolu01 <jaiyeolalulu@gmail.com>
2023-10-04 15:37:27 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,16 +2,16 @@
 #include "main.h"
 
 /**
- *  * main - printd the number of args
- *  * @argc: number of args
- *  * @argv: string array
- *  * Return: 0
- *      
+ *  main - printd the number of args
+ *  @argc: number of args
+ *  @argv: string array
+ * 
+ *  Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argv;
+(void) argv; /*ignore argv*/
 	printf("%d\n", argc - 1);
 	return (0);
 }
