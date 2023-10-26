a33c34e4b4f95fa1d372ec60af7d30c0e1d5a15a
Lolu01 <jaiyeolalulu@gmail.com>
2023-10-25 23:53:23 -0700

    fofo

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,16 +8,12 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
-	
-	new = (listint_t *)malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	
-	new->n = n;
-	new->next = *head;
-	
-	*head = new;
-	
-	return (new);
+listint_t *new;
+new = (listint_t *)malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = *head;
+*head = new;
+return (new);
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,25 +8,20 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node;
-	
-	new_node = (listint_t *)malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	
-	new_node->n = n;
-	new_node->next = NULL;
-	
-	if (*head == NULL)
-		*head = new_node;
-	
-	else
-	{
-		listint_t *last_node = *head;
-		while (last_node->next != NULL)
-			last_node = last_node->next;
-		last_node->next = new_node;
-	}
-	
-	return (new_node);
+listint_t *new_node;
+new_node = (listint_t *)malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->next = NULL;
+if (*head == NULL)
+*head = new_node;
+else
+{
+listint_t *last_node = *head;
+while (last_node->next != NULL)
+last_node = last_node->next;
+last_node->next = new_node;
+}
+return (new_node);
 }
