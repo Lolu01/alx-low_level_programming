850a6c1e5d94c02e40f3f64709195f3274a73a41
Lolu01 <jaiyeolalulu@gmail.com>
2023-10-26 00:19:20 -0700

    ghgi

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,16 +7,14 @@
  */
 int sum_listint(listint_t *head)
 {
-	int sum = 0;
-	listint_t *temp = head;
-	
-	if (head == NULL)
-		return (0);
-	while (temp != NULL)
-	{
-		sum += temp->n;
-		temp = temp->next;
-	}
-	
-	return (sum);
+int sum = 0;
+listint_t *temp = head;
+if (head == NULL)
+return (0);
+while (temp != NULL)
+{
+sum += temp->n;
+temp = temp->next;
+}
+return (sum);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,43 +9,33 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp = *head;
-	unsigned int i = 0;
-	
-	if (idx == 0)
-	{
-		listint_t *new;
-		new = (listint_t *)malloc(sizeof(listint_t));
-		
-		if (new == NULL)
-			return (NULL);
-		
-		new->n = n;
-		new->next = *head;
-		
-		*head = new;
-		return (new);
-	}
-	
-	while (temp != NULL)
-	
-	{
-		if (i == idx - 1)
-		{
-			listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
-			
-			if (new_node == NULL)
-				return (NULL);
-			
-			new_node->n = n;
-			new_node->next = temp->next;
-			temp->next = new_node;
-			return (new_node);
-		}
-		
-		temp = temp->next;
-		i++;
-	}
-	
-	return (NULL);
+listint_t *temp = *head;
+unsigned int i = 0;
+if (idx == 0)
+{
+listint_t *new;
+new = (listint_t *)malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = *head;
+*head = new;
+return (new);
+}
+while (temp != NULL)
+{
+if (i == idx - 1)
+{
+listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->next = temp->next;
+temp->next = new_node;
+return (new_node);
+}
+temp = temp->next;
+i++;
+}
+return (NULL);
 }
